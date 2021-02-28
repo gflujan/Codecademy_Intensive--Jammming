c1947d49324b596beb94930225e1cea7d086aad1
Gabriel F. Lujan <gflujan@gmail.com>
2021-02-28 11:24:25 -0800

    update `myRedirectUri` to prep for Heroku deployment;

--- src/config/redirect.js
+++ src/config/redirect.js
@@ -1,4 +1,4 @@
 module.exports = {
-   myRedirectUri: 'http://localhost:3000/', // Local Machine URI
-   // myRedirectUri: 'https://gfl-jammming.herokuapp.com/', // Prod Machine URI
+   // myRedirectUri: 'http://localhost:3000/', // Local Machine URI
+   myRedirectUri: 'https://gfl-jammming.herokuapp.com/', // Prod Machine URI
 };
