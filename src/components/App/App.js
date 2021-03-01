55fbd45e325dcf95c77d7d94ffb7bace105fb08c
Gabriel F. Lujan <gflujan@gmail.com>
2021-02-28 21:34:02 -0800

    update playlist placeholder name & test Netlify auto-deploy;

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
       this.state = {
          playlistName: "",
-         playlistNamePlaceholder: "Playlist name",
+         playlistNamePlaceholder: "Enter a name...",
          playlistTracks: [],
          searchResults: [],
       };
